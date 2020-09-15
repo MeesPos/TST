9ffbb841d4cc99b0a824c1fab9b4cc6e88579c92
MeesPos <meespostma_@hotmail.com>
2020-09-15 10:00:49 +0200

    Opdracht 2

---
+++ opdr2/README.md
@@ -0,0 +1,16 @@
+# De Units
+
+## In Vooraad function:
+GetVoorraad()
+getArtikel()
+showVoorraad()
+verlaagVoorraad()
+verhoogVoorraad()
+verhoogMeerdereVoorraad()
+verlaagMeerdereVoorraad()
+
+## In Auto function:
+
+getBrand()
+getYear()
+showDetails()
---
+++ opdr2/script.js
@@ -0,0 +1,48 @@
+function Auto(merk, bouwjaar) {
+
+    let brand = merk;
+    let year = bouwjaar;
+
+    this.getBrand = function () {
+        return brand;
+    }
+   
+    this.getYear = function () {
+        return year;
+    }
+   
+    this.showDetails = function () {
+        console.log('Automerk: ' + brand + ' bouwjaar: ' + bouwjaar);
+    }
+   
+}
+
+function Voorraad(item, aantal) {
+    let artikel = item;
+    let voorraad = aantal;
+
+    this.getVoorraad = function () {
+        return voorraad;
+    }
+    this.getArtikel = function () {
+        return artikel;
+    }
+    this.showVoorraad = function () {
+        console.log('De voorraad van : ' + artikel + ' is ' + voorraad);
+    }
+    this.verlaagVoorraad = function () {
+        voorraad = voorraad  -1;
+    }
+
+    this.verhoogVoorraad = function () {
+        voorraad = voorraad  +1;
+    }
+
+    this.verhoogMeerdereVoorraad = function () {
+        voorraad = voorraad  +5;
+    }
+
+    this.verlaagMeerdereVoorraad = function () {
+        voorraad = voorraad  -5;
+    }
+}
